9199db18db6f82c665fcb4f2d88292075502e8f8
Misa <joharymisa8@gmail.com>
2021-12-08 15:45:21 +0100

    group by availability working

--- lib/screens/home_test.dart
+++ lib/screens/home_test.dart
@@ -11,9 +11,63 @@ class HomePageTest extends StatefulWidget {
 }
 
 class _HomePageTestState extends State<HomePageTest> {
+  bool _groupByAvaibility = true;
+  bool _showEmpty = false;
+  final List<String> _day = [
+    'Lundi',
+    'Mardi',
+    'Mercredi',
+    'Jeudi',
+    'Vendredi',
+    'Samedi',
+    'Dimanche'
+  ];
+  BenevoleFile _benevole = BenevoleFile(
+      id: [],
+      name: [],
+      number: [],
+      email: [],
+      adresse: [],
+      profession: [],
+      availability: []);
+
+  final List<MaterialColor> _color = [
+    Colors.cyan,
+    Colors.purple,
+    Colors.orange,
+    Colors.lightGreen,
+    Colors.pink,
+    Colors.brown,
+    Colors.amber
+  ];
   @override
   Widget build(BuildContext context) {
     // final _benevole = Provider.of<BenevoleNotifier>(context).readBenevoles();
+
+    context.select((BenevoleNotifier controller) =>
+        controller.benevole != null ? _benevole = controller.benevole : null);
+
+    List<Benevole> benTemp = List.generate(
+      _benevole.id.length,
+      (index) => Benevole(
+        id: _benevole.id[index],
+        name: _benevole.name[index],
+        adresse: _benevole.adresse[index],
+        number: _benevole.number[index],
+        availability: _benevole.availability[index],
+        profession: _benevole.profession[index],
+      ),
+    ).toList();
+
+    List _days = [
+      benTemp.where((element) => element.availability == 'Lundi').toList(),
+      benTemp.where((element) => element.availability == 'Mardi').toList(),
+      benTemp.where((element) => element.availability == 'Mercredi').toList(),
+      benTemp.where((element) => element.availability == 'Jeudi').toList(),
+      benTemp.where((element) => element.availability == 'Vendredi').toList(),
+      benTemp.where((element) => element.availability == 'Samedi').toList(),
+      benTemp.where((element) => element.availability == 'Dimanche').toList(),
+    ];
 
     return Scaffold(
       appBar: AppBar(
@@ -28,14 +82,107 @@ class _HomePageTestState extends State<HomePageTest> {
           )
         ],
       ),
-      body: context
-          .select((BenevoleNotifier controller) => controller.benevole != null
-              ? ListView.builder(
+      body: context.select((BenevoleNotifier controller) => controller
+                  .benevole !=
+              null
+          ? _groupByAvaibility
+              ? SingleChildScrollView(
+                  child: Column(
+                      children: List.generate(
+                    _days.length,
+                    (i) => _days[i].length == 0
+                        ? _showEmpty
+                            ? Container(
+                                margin: EdgeInsets.all(10),
+                                height: 70,
+                                child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Expanded(
+                                          flex: 1,
+                                          child: Container(
+                                            child: Center(
+                                              child: Text(
+                                                _day[i],
+                                                overflow: TextOverflow.fade,
+                                                softWrap: false,
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .caption
+                                                    .copyWith(
+                                                        color: Colors.white,
+                                                        fontSize: 15),
+                                              ),
+                                            ),
+                                            color: _color[i],
+                                            height: double.infinity,
+                                          )),
+                                      Expanded(
+                                          flex: 5,
+                                          child: Container(
+                                            color: Colors.black12,
+                                            child: Center(
+                                                child: Text(
+                                              'Aucune personne Disponible',
+                                              style: TextStyle(
+                                                  fontStyle: FontStyle.italic),
+                                            )),
+                                          )),
+                                    ]),
+                              )
+                            : Container()
+                        : Container(
+                            margin: EdgeInsets.all(10),
+                            height: _days[i].length * 70.0,
+                            child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                      flex: 1,
+                                      child: Container(
+                                        child: Center(
+                                          child: Text(
+                                            _days[i][0].availability,
+                                            overflow: TextOverflow.fade,
+                                            softWrap: false,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .caption
+                                                .copyWith(
+                                                    color: Colors.white,
+                                                    fontSize: 15),
+                                          ),
+                                        ),
+                                        color: _color[i],
+                                        height: double.infinity,
+                                      )),
+                                  Expanded(
+                                    flex: 5,
+                                    child: ListView.builder(
+                                      itemCount: _days[i].length,
+                                      itemBuilder: (ctx, index) => ListTile(
+                                        onTap: () {
+                                          Navigator.of(context).pushNamed(
+                                              DetailScreen.routeName,
+                                              arguments: _days[i][index].id);
+                                        },
+                                        title: Text(_days[i][index].name),
+                                        subtitle: Text(_days[i][index].number),
+                                      ),
+                                    ),
+                                  ),
+                                ]),
+                          ),
+                  ).toList()),
+                )
+              : ListView.builder(
                   itemBuilder: (context, index) {
                     return Column(children: [
                       ListTile(
                         onTap: () {
-                          Navigator.of(context).pushNamed(DetailScreen.routeName,
+                          Navigator.of(context).pushNamed(
+                              DetailScreen.routeName,
                               arguments: controller.benevole.id[index]);
                         },
                         title: Text(controller.benevole.name[index]),
@@ -47,7 +194,11 @@ class _HomePageTestState extends State<HomePageTest> {
                     ]);
                   },
                   itemCount: controller.benevole.name.length)
-              : Container(child: Center(child: Text('Nothing to print here'),),)),
+          : Container(
+              child: Center(
+                child: Text('Nothing to print here'),
+              ),
+            )),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
@@ -59,7 +210,6 @@ class _HomePageTestState extends State<HomePageTest> {
 }
 
 class Search extends SearchDelegate {
-
   // bool groupBy;
 
   // Search(this.groupBy);
@@ -99,16 +249,16 @@ class Search extends SearchDelegate {
       itemCount: result.length,
       itemBuilder: (context, index) {
         return Column(children: [
-                  ListTile(
-                    onTap: () {
-                      // Navigator.of(context).pushNamed(DetailScreen.routeName,
-                      //     arguments: result[index].id);
-                    },
-                    title: Text(result[index]),
-                    subtitle: Text(numberSuggest[index]),
-                  ),
-                  Divider()
-                ]);
+          ListTile(
+            onTap: () {
+              // Navigator.of(context).pushNamed(DetailScreen.routeName,
+              //     arguments: result[index].id);
+            },
+            title: Text(result[index]),
+            subtitle: Text(numberSuggest[index]),
+          ),
+          Divider()
+        ]);
         // return ListTile(
         //   title: result[index].name,
         //   subtitle: result[index].number,
@@ -121,23 +271,25 @@ class Search extends SearchDelegate {
     // show when someone searches for something
 
     BenevoleFile _benevole = BenevoleFile(
-      id: [],
-      name: [],
-      number: [],
-      email: [],
-      adresse: [],
-      profession: [],
-      availability: []);
+        id: [],
+        name: [],
+        number: [],
+        email: [],
+        adresse: [],
+        profession: [],
+        availability: []);
 
-    context.select(
-        (BenevoleNotifier controller) => controller.benevole != null? _benevole = controller.benevole : null);
- 
-    var result = _benevole.name.where((element) => element.contains(RegExp("$query", caseSensitive: false))).toList();
+    context.select((BenevoleNotifier controller) =>
+        controller.benevole != null ? _benevole = controller.benevole : null);
+
+    var result = _benevole.name
+        .where((element) =>
+            element.contains(RegExp("$query", caseSensitive: false)))
+        .toList();
     var index = [];
     List idSuggest = [];
-    
 
-    for(var i = 0; i<result.length; i++){
+    for (var i = 0; i < result.length; i++) {
       index.add(_benevole.name.indexWhere((element) => element == result[i]));
       idSuggest.add(_benevole.id[index[i]]);
       numberSuggest.add(_benevole.number[index[i]]);
@@ -149,10 +301,8 @@ class Search extends SearchDelegate {
             itemCount: result.length,
             itemBuilder: (context, index) => ListTile(
               onTap: () {
-                Navigator.of(context).pushNamed(
-                  DetailScreen.routeName,
-                    arguments: idSuggest[index]
-                    );
+                Navigator.of(context).pushNamed(DetailScreen.routeName,
+                    arguments: idSuggest[index]);
                 // showResults(context);
               },
               title: Text(result[index]),
